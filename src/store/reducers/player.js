9d7c87e5a768949bd47eb2729b9c44e896b0244d
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-09-08 17:51:37 -0300

    requisito 3 e4

--- src/store/reducers/player.js
+++ src/store/reducers/player.js
@@ -1,3 +1,5 @@
+import { USER_LOGIN, API_FAIL, API_REQUEST } from '../actions';
+
 const INITIAL_STATE = {
   name: '',
   assertions: 0,
@@ -7,6 +9,19 @@ const INITIAL_STATE = {
 
 const playerReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case USER_LOGIN: return {
+    ...state,
+    name: action.payload.name,
+    gravatarEmail: action.payload.email,
+    token: action.trivia,
+  };
+  case API_REQUEST: return {
+    ...state,
+    loading: true,
+  };
+  case API_FAIL: return {
+    ...state,
+  };
   default:
     return state;
   }
