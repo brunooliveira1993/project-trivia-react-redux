7d542d4f7a2996ea48ae1ed9993ea687a0ab218e
Bruno <brunocfaraco@hotmail.com.br>
2022-09-15 17:06:43 -0300

    mudança do app de func para class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
@@ -7,14 +7,18 @@ import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 
-export default function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route path="/settings" component={ Settings } />
-      <Route path="/game" component={ Game } />
-      <Route path="/feedback" component={ Feedback } />
-      <Route path="/ranking" component={ Ranking } />
-    </Switch>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
+        <Route path="/game" component={ Game } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
+      </Switch>
+    );
+  }
 }
+
+export default App;
